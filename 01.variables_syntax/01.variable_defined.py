1804c4a9ec4b458ec99f862456a2c83d7afc8143
Distantskyline <zhanggengheng@qq.com>
2019-08-05 16:59:29 +0800

    python 第一天学习

---
+++ 01.variables_syntax/01.variable_defined.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+#coding: utf8
+#
+#author:Distantskyline
+#date:1908
+# :
+
+# for n in range(0,201):
+#     if n % 2 == 1 and n % 3 == 2 and n % 5 == 4 and n % 6 == 5  and n % 7 == 0:
+#         print(n)
+
+
+#for循环遍历100以下的素数
+# i=2
+# for i in range(2,100):
+#     n=2
+#     for n in range(2,i):
+#         if i % n == 0:
+#             break
+#     else:
+#         print(i)
+
+#输入a+b得c
+# a = int(input('请输入a：' ))
+# b = int(input('请输入b: ' ))
+# c = a + b
+# print('c=',c)
+
+
+#比较两数的大小
+# a = int(input('请输入a:'))
+# b = int(input('请输入b:'))
+# if a == b:
+#      print("a等于b")
+#
+# elif a <= b:
+#     print("a小于b")
+# elif a >= b:
+#     print("a大于b")
+
+
+
