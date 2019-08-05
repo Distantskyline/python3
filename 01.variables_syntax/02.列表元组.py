8dd2fd0baf090e313bdb96597bec26987bd5e648
Distantskyline <zhanggengheng@qq.com>
2019-08-05 17:28:56 +0800

    '123'

--- 01.variables_syntax/02.列表元组.py
+++ 01.variables_syntax/02.列表元组.py
@@ -1,5 +1,12 @@
-print('''
-cpu:{}
-men:{}
-disk{}
-'''.format('70%','50%','20%'))
+# print('''
+# cpu:{}
+# men:{}
+# disk{}
+# '''.format('70%','50%','20%'))
+#
+
+list01=[1,1.2,2.23,"hello","world",True]
+tuple=(1,2.33,4.2,'hello','world',False)
+
+list01.append('你好！',)
+print(list01)
