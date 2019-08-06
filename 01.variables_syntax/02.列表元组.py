98eab81ae6cb61a5542ec53a3bbecef900b567ab
Distantskyline <zhanggengheng@qq.com>
2019-08-06 17:22:26 +0800

    python 第一天学习

--- 01.variables_syntax/02.列表元组.py
+++ 01.variables_syntax/02.列表元组.py
@@ -42,32 +42,55 @@
 # print('no data available for person with id: {}, name: {}'.format(ID, name))
 
 ######  反转 句子
-# list01 = ['this','is','my','house']
+# str = "this is my house"
+# list01 = str.split()
 # list01.reverse()
 # print(list01)
+# str02 = ''
+# for i in range(len(list01)):
+#     str01 = list01[i]
+#     if i != len(list01):
+#         str02 += (str01 + ' ')
+# print(str02)
+
+
+# i = 0
+# str02 = ''
+# while i < len(list01):
+#     str01 = list01[i]
+#     str02 += str01 + ' '
+#     i += 1
+#     if i == len(list01):
+#         print(str02)
+# str03=str02.rstrip()
+# print(str03)
+
+
 
 ######  敏感字符替换，如果存在敏感词混，直接替换为*
 # str = 'hello world for you'
-# # str01 = 'ol'
-# # n = list(str01)
-# if () in str:
+# str01 = 'ol'
+# n = list(str01)
+# if 'o' in str:
 #         str02 = str.replace('o','*')
 #         print(str02)
-# # else:
+# else:
 #     print(str02)
 
 ######  把数字提交到列表中
 # tuple = ('string','world',1,2,3,4,5,6,9,10)
-# # 1.
 # i = 0
 # n = len(tuple)
-# print(n)
+# list01 = []
 # while i < n :
-#     print(tuple[i])
+#     a = tuple[i]
+#     b = int
 #     i += 1
-# # 2.
-# list = tuple[3:]
-# print(list)
+#     if type(a) == b:
+#         q = tuple[i-1]
+#         list01.append(q)
+#         if i == n:
+#             print(list01)
 
 ######  转换列表中的元组为普通形式
 # list01 = ["string", "tuple", "list", (1, 2, 3, 4, 5), [6, 7]]
@@ -96,3 +119,14 @@
 
 ######  for 循环数字排序
 list01 = [23,12,15,11,29,24,57,21,80,99,45]
+# for i in range(len(list01)):
+#     for j in range(len(list01) -i -1):
+#         if list01[j] > list01 [j+1]:
+#             list01[j],list01[j+1] = list01[j+1],list01[j]
+# print(list01)
+
+# for i in range(len(list01)):
+#     for j in range(len(list01)-i-1):
+#         if list01[j] > list01[j+1]:
+#             list01[j],list01[j+1] = list01[j+1],list01[j]
+# print(list01)
