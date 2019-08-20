0ed725b0ab99d11d36b97fd7c4ad1f96be27cc6a
Distantskyline <zhanggengheng@qq.com>
2019-08-20 14:12:31 +0800

    python

--- 2.面向对象/5/error.py
+++ 2.面向对象/5/error.py
@@ -5,4 +5,28 @@
 #author:Distantskyline
 
 
-print(1/0)
+import random
+
+
+def verificationCode(number):
+    """
+    product verification code of user input number
+    :param number: verification code's number
+    :return: verification code
+    """
+    vCode = ''
+    for n in range(number):
+        nums = random.randint(0, 9)
+        word = chr(random.randint(65, 90))
+        middle = random.choice([nums, word])
+        vCode += str(middle)
+    return vCode
+
+
+verCode = verificationCode(4)
+print(verCode)
+userInput = input('Please input verification code: ')
+if userInput.lower() != verCode.lower():
+    print('your Stupid !')
+else:
+    print('please load...')
