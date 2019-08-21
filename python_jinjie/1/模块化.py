0122f2acf48f0926e0ef47713ff109d3687518cd
Distantskyline <zhanggengheng@qq.com>
2019-08-21 21:40:23 +0800

    module

--- python_jinjie/1/模块化.py
+++ python_jinjie/1/模块化.py
@@ -4,16 +4,19 @@
 #author:Distantskyline
 #usage:
 
-# variable = __import__('python_jinjie.wheel.log_analysis',fromlist=True)
-#
-# gongneng = input('gongneng: ')
-# # fangfa = input('')
-#
-# if hasattr(variable,gongneng):
-#     func = getattr(variable,gongneng)
-#     ips1 = func('../wheel/access_log')
-#     print(ips1)
 
+###                                     f反射导入方法
+variable = __import__('python_jinjie.wheel.log_analysis',fromlist=True)
+
+gongneng = input('gongneng: ')
+# fangfa = input('')
+
+if hasattr(variable,gongneng):
+    func = getattr(variable,gongneng)
+    ips1 = func('../wheel/access_log')
+    print(ips1)
+
+###                                   from导入模块方法
 from python_jinjie.wheel import log_analysis
 
 ipslist = log_analysis.loganalysis('../wheel/access_log')
---
+++ python_jinjie/3/paramiko_test.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+#
+#author:Distantskyline
+#date:190821-20:44
+#usage:
+
+# ##                        密码链接远程服务器
+import paramiko
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(
+    hostname='192.168.52.135',
+    port=22,
+    username='root',
+    password='1'
+)
+
+stdin , stdout , stderr = client.exec_command(command='ls',timeout=2)
+print(stdout.read().decode('utf-8'))
+client.close()
+
+##                            密钥对链接远程
+import paramiko
+key = paramiko.RSAKey.from_private_key_file('C:\\Users\Administrator\.ssh\id_rsa')
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(
+    hostname='192.168.52.135',
+    port=22,
+    username='root',
+    pkey=key
+)
+
+stdin , stdout , stderr = client.exec_command('ls')
+print(stdout.read().decode('utf-8'))
+client.close()
+
+
+##                                 通过通道链接远程主机
+import paramiko
+key = paramiko.RSAKey.from_private_key_file('C:\\Users\Administrator\.ssh\id_rsa')
+transport = paramiko.Transport(('192.168.52.135',22))
+transport.connect(username='root',pkey=key)
+client = paramiko.SSHClient()
+client._transport = transport
+stdin , stdout, stderr = client.exec_command('ls')
+print(stdout.read().decode('utf-8'))
+transport.close()
